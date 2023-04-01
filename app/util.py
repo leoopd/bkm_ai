6183c91af6fc84d6c8ecccfdaf9265c4c179484b
leo <leo.opdenplatz@gmail.com>
2023-04-01 20:17:45 +0200

    added newsletter_dict_maker()
    creates a dictionary from the extracted title and body
    need to add functionality that handles cases where body and/or title cant be extracted (inconsistency in the format ie)

--- app/util.py
+++ app/util.py
@@ -4,6 +4,7 @@
 
 from bs4 import BeautifulSoup
 import re
+import os
 
 def newsletter_parser_body(path):
     with open(path, 'rb') as f:
@@ -16,7 +17,6 @@ def newsletter_parser_body(path):
         # Removes occurrences of punctuation (preserves one occurrence), removes extra spaces.
         text = re.sub(r'([^\w\s])\1+', r'\1 ', text)
         text = re.sub(r'(\s+)([^\w\s])*\s*', r'\2 ', text)
-
 
         # Inserts the missing spaces between words (can contain äöüßÜÖÄ) and after punctuation.
         text = re.sub(r'(?<=[.,?!])(?=[^\s\d])(?!$)|(?<=[a-zäöüß])(?=[A-ZÜÖÄ])', r' ', text)
@@ -38,6 +38,21 @@ def newsletter_parser_title(path):
                 img_title = soup.find_all('img')[1].get('alt')
                 return img_title
             except IndexError:
-                print(f'no title or img_title for file: {path}')
+                return ''
 
-print(newsletter_parser_title('/home/leo/coding/python/bkm_ai/test_newsl/Strato Newsletter3.html'))
+def newsletter_dict_maker(folder_path):
+    prompts = {}
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        if os.path.isfile(file_path):
+            text = newsletter_parser_body(file_path)
+            title = newsletter_parser_title(file_path)
+            if title:
+                prompts[title] = text
+    return prompts
+
+prompts = newsletter_dict_maker('/home/leo/coding/python/bkm_ai/test_newsl')
+
+for prompt in prompts.items():
+    print(prompt)
+    print('\n###\n')
