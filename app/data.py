40edea09cc323913a5782306c3b16daa5beba919
leo <leo.opdenplatz@gmail.com>
2023-04-02 21:15:20 +0200

    changed the implemented functions to be more versatile
    the newsletters dont follow a strict format, so the funcs need to handle different cases

---
+++ app/data.py
@@ -0,0 +1,30 @@
+import imaplib
+import email
+from getpass import getpass
+
+# Login to your email account
+# username = input('gmail: ')
+# password = getpass('password: ')
+username = 'ralf.hinberger.bkm@web.de'
+password = 'hUo8#&03M#PD'
+imap_host = 'imap.web.de'
+imap_port = 993
+
+mail = imaplib.IMAP4_SSL(imap_host, imap_port)
+mail.login(username, password)
+mail.select('inbox')
+
+status, messages = mail.search(None, 'SUBJECT "WG"')
+
+for message_id in messages[0].split():
+    _, msg_data = mail.fetch(message_id, "(RFC822)")
+    email_body = msg_data[0][1]
+    mail_message = email.message_from_bytes(email_body)
+
+    for part in mail_message.walk():
+        if part.get_content_type() == 'text/plain':
+            content = part.get_payload(decode=True)
+            print(content.decode())
+
+            with open(f'/home/leo/coding/python/bkm_ai/test_newsl/newsletter_{message_id}.txt', 'wb') as f:
+                f.write(content)
--- app/util.py
+++ app/util.py
@@ -1,5 +1,5 @@
 # ToDo:
-#   - add functionality to handle cases where title and or body cant be extracted due to format inconsistencies
+#   - the textbodies end in different 
 # Bugs:
 #   - does not properly decode utf-8 if type of file unicode text
 #   - splits .de-Domains etc. when it shouldnt
@@ -9,51 +9,73 @@ import re
 import os
 import csv
 
+path1 = '/home/leo/coding/python/bkm_ai/test_newsl/20230113-So schützen Sie Ihre Daten-18.html'
+path2 = '/home/leo/coding/python/bkm_ai/test_newsl/20220724-Ihre Vorteile als STRATO Kunde - jetzt ansehen!-45.html'
+path3 = '/home/leo/coding/python/bkm_ai/test_newsl/20230402-WG_ Welt-Backup-Tag_ 50 % Rabatt auf das HiDrive Geräte-Backup-86.html'
+
+folder_path = '/home/leo/coding/python/bkm_ai/test_newsl'
+
 def newsletter_parser_body(path):
     with open(path, 'rb') as f:
         soup = BeautifulSoup(f, 'html.parser')
         tmp = soup.get_text()
+        # Removes occurrences of punctuation (preserves one occurrence), removes extra spaces
+        # and inserts the missing spaces between words (can contain äöüßÜÖÄ) and after punctuation.
+        tmp = re.sub(r'([^\w\s])\1+', r'\1 ', tmp)
+        tmp = re.sub(r'(\s+)([^\w\s])*\s*', r'\2 ', tmp)
+        tmp = re.sub(r'(?<=[.,?!])(?=[^\s\d])(?!$)|(?<=[a-zäöüß])(?=[A-ZÜÖÄ])', r' ', tmp)
 
-        # Extracts the text body (always starts with 'Hallo' and ends with 'STRATO Team').
-        text = tmp[tmp.find('Hallo'):tmp.find('STRATO Team')+11]
+        # Extracts the text body.
+        text = tmp[tmp.find('Hallo'):tmp.find('30 Tage')+11]
+        if not text:
+            text = tmp[tmp.find('Sehr geehrt'):tmp.find('30 Tage')+11]
 
-        # Removes occurrences of punctuation (preserves one occurrence), removes extra spaces.
+        # Same re as above.
         text = re.sub(r'([^\w\s])\1+', r'\1 ', text)
         text = re.sub(r'(\s+)([^\w\s])*\s*', r'\2 ', text)
-
-        # Inserts the missing spaces between words (can contain äöüßÜÖÄ) and after punctuation.
         text = re.sub(r'(?<=[.,?!])(?=[^\s\d])(?!$)|(?<=[a-zäöüß])(?=[A-ZÜÖÄ])', r' ', text)
-
         return text
-    
-def newsletter_parser_title(path):
-    with open(path, 'rb') as f:
-        soup = BeautifulSoup(f, 'html.parser')
 
+def newsletter_parser_title(path):
         # Tries to find the title as a header.
         try:
-            title = soup.find_all('span', style="font-size:16px;")[0].text
-            return title
-        except IndexError:
-
+            with open(path, 'rb') as f:
+                soup = BeautifulSoup(f, 'html.parser')
+                title_tag = soup.find_all('span', style="font-size:16px;")
+                if not title_tag:
+                    title_tag = soup.find_all('span', style="font-size:19px;")
+                return title_tag[0].text
+                
+        except:
             # Looks for a Title embedded into an image.
             try:
-                img_title = soup.find_all('img')[1].get('alt')
-                return img_title
-            except IndexError:
+                with open(path, 'rb') as f:
+                    soup = BeautifulSoup(f, 'html.parser')
+                    img_title = soup.find_all('img')[1].get('alt')
+                    if img_title == 'STRATO AG':
+                        img_title = soup.find_all('img')[2].get('alt')
+                    return img_title
+                    
+            except:
                 return ''
 
 def newsletter_dict_maker(folder_path):
     prompts = {}
 
-    # Iterates over all files in the folder specified
+    # Iterates over all files in the folder specified.
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
         if os.path.isfile(file_path):
             text = newsletter_parser_body(file_path)
             title = newsletter_parser_title(file_path)
-            if title:
+            if title and text:
                 prompts[title] = text
+            elif title:
+                print(f'{folder_path+"/"+filename} missing text')
+            else:
+                print(f'{folder_path+"/"+filename} missing title')
+        else:
+            print(f'{folder_path+"/"+filename} not a file')
     return prompts
 
 def prompts_to_csv(prompts_dict):
