692b856021b9c33ce2a57e85fc5cb48720ab9c78
leo <leo.opdenplatz@gmail.com>
2023-04-02 21:36:59 +0200

    added comments to data.py

--- app/data.py
+++ app/data.py
@@ -2,18 +2,21 @@ import imaplib
 import email
 from getpass import getpass
 
-# Login to your email account
+# Webmailer Config.
 username = input('gmail: ')
 password = getpass('password: ')
 imap_host = 'imap.web.de'
 imap_port = 993
 
+# Logging into the email.
 mail = imaplib.IMAP4_SSL(imap_host, imap_port)
 mail.login(username, password)
 mail.select('inbox')
 
+# Filtering emails with the Subject specified.
 status, messages = mail.search(None, 'SUBJECT "WG"')
 
+# Decoding the text contents of the email and writing it to disc.
 for message_id in messages[0].split():
     _, msg_data = mail.fetch(message_id, "(RFC822)")
     email_body = msg_data[0][1]
